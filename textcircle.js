f8377544593f87d6db4c49cf306b02f9337c52a7
Wesley <wesleydmuniz@gmail.com>
2017-03-12 20:15:37 -0300

    Adding packages twbs:bootstrap, accounts-password and ian:accounts-ui-bootstrap-3

--- textcircle.js
+++ textcircle.js
@@ -1,6 +1,39 @@
 this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient) {
+
+  Accounts.ui.config({
+    extraSignupFields: [{
+        fieldName: 'first-name',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        validate: function(value, errorFunction) {
+          if (!value) {
+            errorFunction("Please write your first name");
+            return false;
+          } else {
+            return true;
+          }
+        }
+      },
+      {
+        fieldName: 'last-name',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+      },
+    ],
+	  requestPermissions: {
+	    facebook: ['user_likes'],
+	    github: ['user', 'repo']
+	  },
+	  requestOfflineToken: {
+	    google: true
+	  },
+	  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+	});
+
 // find the first document in the Documents colleciton and send back its id
   Template.editor.helpers({
     docid:function(){
@@ -11,7 +44,7 @@ if (Meteor.isClient) {
       else {
         return undefined;
       }
-    }, 
+    },
     // template helper that configures the CodeMirror editor
     // you might also want to experiment with the ACE editor
     config:function(){
@@ -20,15 +53,15 @@ if (Meteor.isClient) {
         editor.on("change", function(cm_editor, info){
           //console.log(cm_editor.getValue());
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
-        });        
+        });
       }
-    }, 
+    },
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // startup code that creates a document in case there isn't one yet. 
+    // startup code that creates a document in case there isn't one yet.
     if (!Documents.findOne()){// no documents yet!
         Documents.insert({title:"my new document"});
     }
